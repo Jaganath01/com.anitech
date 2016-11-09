9d00119a05c1671260bebcd774ab0944ba4e7ac6
Jag <jagannath01@gmail.com>
2016-11-09 11:21:52 +0530

    Initial Version

---
+++ src/main/java/com/anitech/javaBrushUp.java
@@ -0,0 +1,11 @@
+package com.anitech;
+
+/**
+ * Created by Jag on 11/9/16.
+ */
+public class javaBrushUp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
